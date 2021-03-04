2a58e329c6dcbfe5412241a4b4c411c3ca697a51
Suman Thakur <t_suman1721@gmail.com>
2021-03-04 12:42:25 +0530

    error in joining

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import '../../App.css';
-import M from 'materialize-css';
+import React from "react";
+import "../../App.css";
+import M from "materialize-css";
+import { react, useState, useEffect } from "react";
+import axios from "axios";
 
+const Home = () => {
+  // console.warn("okkkk");
+  const [loggedin, setloggedin] = useState(1);
 
-const Home=()=>{
-	return (
-		<div className="hbg" style={{backgroundImage:"url('./images/bg1.jpg')"}}>
-		</div>
-	);
-}
+  useEffect(() => {
+    axios.get("http://localhost:5000/isloggedin").then((props) => {
+      setloggedin(props.data.loggedin);
+    });
+  }, []);
 
-export default Home; 
+  let x = (
+    <div
+      className="hbg"
+      style={{ backgroundImage: "url('./images/bg1.jpg')" }}
+    ></div>
+  );
+
+  if (loggedin === 0) return <h1>NOT LOGGEDIN</h1>;
+  else return x;
+  //   return <h1>okkkkkkkkkkkkkkkkkkk</h1>;
+};
+
+export default Home;
